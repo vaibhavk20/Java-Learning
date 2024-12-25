d5d4718ea7fbab1dfd9d3cc5adcd18a18d3a7ef6
vaibhavk20 <vaibhav.2071998@gmail.com>
2024-12-25 20:48:05 +0530

    Learn Inheritance Concept.

---
+++ AdvCalc.java
@@ -0,0 +1,16 @@
+public class AdvCalc extends Calc{
+    public int mul(int n1, int n2){
+        return n1 * n2;
+    }
+
+    public int div(int n1, int n2){
+        return n1 / n2;
+    }
+    public static void main(String[] args) {
+        
+    }
+    /*
+     * AdvCalc is the called Sub class
+     * Calc is the Super Class
+     */
+}
---
+++ Inheritance.java
@@ -0,0 +1,17 @@
+public class Inheritance {
+    public static void main(String[] args) {
+        AdvCalc obj1 = new AdvCalc();
+
+        int r1 = obj1.add(4,5);
+        int r2 = obj1.mul(4,5);
+        System.out.println("additiong is:" + r1 + " " + r2);
+
+    }
+    /*
+      * inheritance is used for the reduce code redundancy.
+      * we call the any class method in the other class with help of the extend keyword.
+      * Inheritance laptop is computer it mean laptop has all features of the computer.
+      * we can also use the multilevel inheritance. calc ==> advCalc ==> scientificCal etc.
+      * java not supports the multiple inheritance.
+      */
+}
